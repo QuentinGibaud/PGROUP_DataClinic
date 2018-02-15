87dd6941300bf82ff989f9dc86fa16b96bb0fe50
QuentinGibaud <quentgib@gmail.com>
2018-02-15 18:39:02 +0100

    Change Controller to make it works and change css and iframe

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,14 +6,14 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class HomeController extends Controller
+class DefaultController extends Controller
 {
     /**
-     * @Route("/home", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('index.html.twig');
+        return $this->render('default/index.html.twig');
     }
 }
