7c5dbba83838334b2084d147afd833df4350177e
hortich <hortense.mahon@thetribe.io>
2018-03-09 16:50:56 +0100

    cgu and legal pages

---
+++ src/AppBundle/Controller/UseTermsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UseTermsController extends Controller
+{
+    /**
+     * @Route(
+     *    "/cgu",
+     *    name="app.use_terms"
+     * )
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('use_terms.html.twig');
+    }
+}
