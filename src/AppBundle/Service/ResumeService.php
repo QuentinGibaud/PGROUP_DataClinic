4b1aa49791e6b7a9ae924954cbd2aede5ea626ef
QuentinGibaud <quentgib@gmail.com>
2018-03-23 15:01:07 +0100

    Create a property file with the access to the database

--- src/AppBundle/Service/ResumeService.php
+++ src/AppBundle/Service/ResumeService.php
@@ -2,12 +2,24 @@
 
 namespace AppBundle\Service;
 
-class ResumeService
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ResumeService extends Controller
 {
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     public function getResume()
     {
-
-        $conn = new \PDO("mysql:host=127.0.0.1;dbname=testform", "root", "");
+        $host = $this->container->getParameter('database_host');
+        $namedb = $this->container->getParameter('database_name');
+        $login = $this->container->getParameter('database_user');
+        $password = $this->container->getParameter('database_password');
+        $conn = new \PDO("mysql:host=".$host.";dbname=".$namedb, $login, $password);
         $query = $conn->prepare("SELECT titre_projet, nom_inv, resume_projet FROM infoform");
         $query->setFetchMode(\PDO::FETCH_ASSOC);
         $query->execute();
